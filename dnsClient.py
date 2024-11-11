2c38809de802e06220d0479f496c69aa3f9aa28e
sabamahmood4 <sm12882@nyu.edu>
2024-11-10 23:34:56 -0500

    Update dnsClient.py

--- dnsClient.py
+++ dnsClient.py
@@ -2,15 +2,10 @@ import argparse
 import socket
 import struct
 
-# Useful resources to solve this lab:
-# 1. https://datatracker.ietf.org/doc/html/rfc1034
-# 2. https://datatracker.ietf.org/doc/html/rfc1035
-# 3. Kurose/Ross Book!
-
 def dns_query(type, name, server):
     # Create a UDP socket
     sock = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
-    server_address = (server, 53)  # Enter Port Number
+    server_address = (server, 53)  # Port 53 is the standard DNS port
 
     # Create the DNS query
     ID = 0x1234
@@ -27,51 +22,41 @@ def dns_query(type, name, server):
     NSCOUNT = 0
     ARCOUNT = 0
 
-    # Pack the header
-    header = struct.pack(
-        '!HHHHHH',
-        ID,
-        (QR << 15) | (OPCODE << 11) | (AA << 10) | (TC << 9) | (RD << 8) | (RA << 7) | (Z << 4) | RCODE,
-        QDCOUNT,
-        ANCOUNT,
-        NSCOUNT,
-        ARCOUNT
-    )
+    # Pack header fields
+    header = struct.pack('!HHHHHH', ID, (QR << 15) | (OPCODE << 11) | (AA << 10) | (TC << 9) | (RD << 8) | (RA << 7) | (Z << 4) | RCODE, QDCOUNT, ANCOUNT, NSCOUNT, ARCOUNT)
 
     # Encode the QNAME
-    qname_parts = name.split('.')  # Split the domain name into labels
-    qname_encoded_parts = [struct.pack('B', len(part)) + part.encode('ascii') for part in qname_parts]  # Encode labels
-    qname_encoded = b''.join(qname_encoded_parts) + b'\x00'  # Add the terminating byte
+    qname_parts = name.split('.')  # Split the domain by dots
+    qname_encoded_parts = [struct.pack('B', len(part)) + part.encode('ascii') for part in qname_parts]
+    qname_encoded = b''.join(qname_encoded_parts) + b'\x00'  # Null byte at the end of the domain
 
     # Encode the QTYPE and QCLASS
     if type == 'A':
-        qtype = 1  # Resource Record value for A record
+        qtype = 1  # Resource Record type for IPv4 address
     elif type == 'AAAA':
-        qtype = 28  # Resource Record value for AAAA record
+        qtype = 28  # Resource Record type for IPv6 address
     else:
         raise ValueError('Invalid type')
 
-    qclass = 1  # IN (Internet)
+    qclass = 1  # QCLASS for Internet
 
-    # Create the question section
+    # Combine header and question
     question = qname_encoded + struct.pack('!HH', qtype, qclass)
-
-    # Send the query
     message = header + question
     sent = sock.sendto(message, server_address)
 
-    # Receive the response
+    # Receive the response from the server
     data, _ = sock.recvfrom(4096)  # Buffer size of 4096 bytes
 
     # Parse the response header
     response_header = data[:12]  # DNS header is 12 bytes
     ID, FLAGS, QDCOUNT, ANCOUNT, NSCOUNT, ARCOUNT = struct.unpack('!HHHHHH', response_header)
 
-    # Parse the question section (skip over it, as we already know it matches)
+    # Parse the response question section (same as query)
     response_question = data[12:12 + len(question)]
     assert response_question == question
 
-    # Parse the answer section
+    # Parse the response answer section
     response_answer = data[12 + len(question):]
     offset = 0
     for _ in range(ANCOUNT):
@@ -84,30 +69,30 @@ def dns_query(type, name, server):
                 break
             elif length & 0xc0 == 0xc0:
                 # Pointer
-                pointer = struct.unpack('!H', response_answer[offset-1:offset+1])[0] & 0x3fff
+                pointer = struct.unpack('!H', response_answer[offset - 1:offset + 1])[0] & 0x3fff
                 offset += 1
                 name_parts.append(parse_name(data, pointer))
                 break
             else:
                 # Label
-                label = response_answer[offset:offset+length].decode('ascii')
+                label = response_answer[offset:offset + length].decode('ascii')
                 offset += length
                 name_parts.append(label)
         name = '.'.join(name_parts)
 
         # Parse the type, class, TTL, and RDLENGTH
-        type, cls, ttl, rdlength = struct.unpack('!HHIH', response_answer[offset:offset + 10])
+        rtype, rclass, ttl, rdlength = struct.unpack('!HHIH', response_answer[offset:offset + 10])
         offset += 10
 
         # Parse the RDATA
         rdata = response_answer[offset:offset + rdlength]
         offset += rdlength
 
-        if type == 1:  # A record
+        if rtype == 1:  # A record (IPv4 address)
             ipv4 = socket.inet_ntop(socket.AF_INET, rdata)
             print(f'{name} has IPv4 address {ipv4}')
             return ipv4
-        elif type == 28:  # AAAA record
+        elif rtype == 28:  # AAAA record (IPv6 address)
             ipv6 = socket.inet_ntop(socket.AF_INET6, rdata)
             print(f'{name} has IPv6 address {ipv6}')
             return ipv6
@@ -121,13 +106,13 @@ def parse_name(data, offset):
             break
         elif length & 0xc0 == 0xc0:
             # Pointer
-            pointer = struct.unpack('!H', data[offset-1:offset+1])[0] & 0x3fff
+            pointer = struct.unpack('!H', data[offset - 1:offset + 1])[0] & 0x3fff
             offset += 1
             name_parts.append(parse_name(data, pointer))
             break
         else:
             # Label
-            label = data[offset:offset+length].decode('ascii')
+            label = data[offset:offset + length].decode('ascii')
             offset += length
             name_parts.append(label)
     return '.'.join(name_parts)
